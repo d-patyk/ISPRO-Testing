0f9d0e7dd5a460280b9365c09c0057a65a3ae214
Dmitriy Patyk <nya@nya.nya>
2024-12-19 17:37:26 +0300

    Added vector code

---
+++ vector.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def length2(self):
+        return (self.x * self.x + self.y * self.y)
+
+
