41f03d98588805b20575acce5da148a20a170326
Dmitriy Patyk <nya@nya.nya>
2024-12-19 17:37:38 +0300

    Added vector tests

--- vector.py
+++ vector.py
@@ -19,3 +19,35 @@ class Vector:
         return (self.x * self.x + self.y * self.y)
 
 
+class VectorTestCase(unittest.TestCase):
+    def test_add_zero(self):
+        self.assertEqual(Vector(3, 4) + Vector(0, 0), Vector(3, 4))
+    
+    def test_add_one(self):
+        self.assertEqual(Vector(3, 4) + Vector(1, 1), Vector(4, 5))
+
+    def test_add_two_three(self):
+        self.assertEqual(Vector(3, 4) + Vector(2, 3), Vector(5, 7))
+
+
+    def test_sub_1(self):
+        self.assertEqual(Vector(3, 4) - Vector(2, 3), Vector(1, 1))
+
+    def test_sub_2(self):
+        self.assertEqual(Vector(2, 3) - Vector(3, 4), Vector(-1, -1))
+
+    def test_sub_3(self):
+        self.assertEqual(Vector(3, 4) - Vector(0, 0), Vector(3, 4))
+        
+
+    def test_length2_1(self):
+        v = Vector(3, 4)
+        self.assertEqual(v.length2(), (3 * 3 + 4 * 4))
+    
+    def test_length2_2(self):
+        v = Vector(10, 12)
+        self.assertEqual(v.length2(), (10 * 10 +12 * 12))
+
+    def test_length2_3(self):
+        v = Vector(-1, -10)
+        self.assertEqual(v.length2(), ((-1) * (-1) + (-10) * (-10)))
